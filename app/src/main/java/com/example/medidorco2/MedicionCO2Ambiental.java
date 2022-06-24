5554de0dab9ba848cedf7c2a7a36486111d10039
Fernando López González <fdo.l.glez@gmail.com>
2022-06-24 14:50:04 -0500

    Avance evalución

---
+++ app/src/main/java/com/example/medidorco2/MedicionCO2Ambiental.java
@@ -0,0 +1,14 @@
+package com.example.medidorco2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MedicionCO2Ambiental extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicion_co2_ambiental);
+    }
+}
---
+++ app/src/main/java/com/example/medidorco2/getEvalParametros.java
@@ -0,0 +1,14 @@
+package com.example.medidorco2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class getEvalParametros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_eval_parametros);
+    }
+}
